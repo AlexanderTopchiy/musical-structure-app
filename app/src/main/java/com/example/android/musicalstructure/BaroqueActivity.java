e8d45970f10e3830de764f205f534edc71915b82
Alexander Topchiy <topchiyau@gmail.com>
2018-04-07 13:13:25 +0300

    Created ArrayLists and adapters in Baroque, Classical and Romantic activities

--- app/src/main/java/com/example/android/musicalstructure/BaroqueActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/BaroqueActivity.java
@@ -2,6 +2,9 @@ package com.example.android.musicalstructure;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class BaroqueActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class BaroqueActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_list);
 
+
+        // Create an array of opuses.
+        ArrayList<Opus> opuses = new ArrayList<Opus>();
+
+        // Adding opuses to an array.
+
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        OpusAdapter adapter = new OpusAdapter(this, opuses);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_listyout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
     }
 }
