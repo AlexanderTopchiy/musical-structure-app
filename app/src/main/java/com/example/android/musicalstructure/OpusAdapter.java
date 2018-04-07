52903dd7ca9560e0cfd10ff694131d876111c02a
Alexander Topchiy <topchiyau@gmail.com>
2018-04-07 13:05:15 +0300

    Added OpusAdapter constructor to OpusAdapter class

--- app/src/main/java/com/example/android/musicalstructure/OpusAdapter.java
+++ app/src/main/java/com/example/android/musicalstructure/OpusAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.musicalstructure;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,7 +9,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class OpusAdapter extends ArrayAdapter<Opus> {
+    // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+    // the second argument is used when the ArrayAdapter is populating a single TextView.
+    // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+    // going to use this second argument, so it can be any value. Here, we used 0.
+    public OpusAdapter (Context context, ArrayList<Opus> object) {
+        super(context, 0, object);
+    }
+
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
      *
